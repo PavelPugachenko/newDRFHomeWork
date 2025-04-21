cc3177927747111655dcea98ca6fc35e84730a3c
pavel <pugachenkopavel72@gmail.com>
2025-04-21 17:24:37 +0500

    commit DRF

---
+++ lms/urls.py
@@ -0,0 +1,16 @@
+from lms.apps import LmsConfig
+from rest_framework.routers import DefaultRouter
+from lms.views import CourseViewSet
+
+app_name = LmsConfig.name
+
+router= DefaultRouter()
+
+
+router.register(r'courses', CourseViewSet, basename='courses')
+
+
+urlpatterns = [
+
+] +router.urls
+
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView
 
-# Create your views here.
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+class LessonCreateApiView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+class LessonRetrieveApiView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+class LessonUpdateApiView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+class LessonListApiView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+
+
