9f594f430a48cde4af12f77241b10f26712b31a8
pavel <pugachenkopavel72@gmail.com>
2025-04-28 10:59:38 +0500

    commit DRF 222

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from users.models import Payment, User
+from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import SerializerMethodField
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+class UserSerializer(ModelSerializer):
+    payment_history = PaymentSerializer(many=True, read_only=True, source='payment_set')
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'payment_history',)
+
+
