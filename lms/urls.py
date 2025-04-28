5aafdfad73a1ec967dc307dd15d73934487a55de
pavel <pugachenkopavel72@gmail.com>
2025-04-28 10:48:53 +0500

    commit DRF 1/2

--- lms/urls.py
+++ lms/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from lms.apps import LmsConfig
 from rest_framework.routers import DefaultRouter
-from lms.views import CourseViewSet, HomePageView, LessonListApiView, LessonRetrieveApiView, LessonCreateApiView, \
+from lms.views import CourseViewSet, LessonListApiView, LessonRetrieveApiView, LessonCreateApiView, \
     LessonDestroyApiView, LessonUpdateApiView
 
 app_name = LmsConfig.name
@@ -13,7 +13,6 @@ router= DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
     path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
     path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
--- users/models.py
+++ users/models.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.urls import reverse
 from django.core.validators import RegexValidator
+
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None  # Отключаем поле username
@@ -42,3 +47,35 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+class Payment(models.Model):
+    PAYMENT_CHOICES = [
+        ("BANK_TRANSFER", "Банковский перевод"),
+        ("CASH", "Наличными"),
+    ]
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    payment_date = models.DateField(default=datetime.now, verbose_name="Дата оплаты")
+    paid_course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Оплаченный курс",
+        blank=True,
+        null=True,
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,
+        verbose_name="Оплаченный урок",
+        blank=True,
+        null=True,
+    )
+    amount = models.DecimalField(decimal_places=2, max_digits=20, verbose_name="Сумма")
+    type = models.CharField(
+        max_length=50, choices=PAYMENT_CHOICES, verbose_name="Способ оплаты"
+    )
+
+    def __str__(self):
+        return f"{self.owner} - {self.get_type_display()} - {self.amount}"
