899e8579f9b8d976065a4aae112de9e564ba319d
pavel <pugachenkopavel72@gmail.com>
2025-05-22 22:51:36 +0500

    33/2 troinik 1

--- lms/task.py
+++ lms/task.py
@@ -8,6 +8,8 @@ from lms.models import Subscription
 def mail_update_course_info(course_id):
     """Отправка сообщения об обновлении курса по подписке"""
     subscription_course = Subscription.objects.filter(course=course_id)
+    subscription_course = subscription_course.select_related('user')
+
     print(f"Найдено {len(subscription_course)} подписок на курс {course_id}")
     for subscription in subscription_course:
         print(f"Отправка электронного письма на {subscription.user.email}")
