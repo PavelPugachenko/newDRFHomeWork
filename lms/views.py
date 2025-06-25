632dbce6019e5b323256bc204a6b225968db6e09
pavel <pugachenkopavel72@gmail.com>
2025-06-25 16:47:06 +0500

    ;k11

--- lms/views.py
+++ lms/views.py
@@ -2,29 +2,36 @@ from rest_framework import generics
 from .models import Lesson, Subscription
 from .serializers import LessonSerializer, SubscriptionSerializer
 
+
 class LessonCreateView(generics.CreateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()  # <-- добавьте это
+
 
 class LessonListView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+
 class LessonRetrieveView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+
 
 class LessonUpdateView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+
 class LessonDeleteView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+
 class SubscriptionCreateView(generics.CreateAPIView):
     serializer_class = SubscriptionSerializer
     queryset = Subscription.objects.all()
+
 
 class SubscriptionListView(generics.ListAPIView):
     serializer_class = SubscriptionSerializer
